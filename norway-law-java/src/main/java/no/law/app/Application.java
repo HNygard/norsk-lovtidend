350b28938479728a9a867c255aa1d58678176b17
Hallvard Nygard <hallvard.nygard@gmail.com>
2020-02-26 20:32:05 +0100

    Basic spring boot app

---
+++ norway-law-java/src/main/java/no/law/app/Application.java
@@ -0,0 +1,16 @@
+package no.law.app;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+
+
+}
