d3a4649b7a67850318ece9eed0fc39e3eeee5421
Hallvard Nygard <hallvard.nygard@gmail.com>
2020-02-27 18:13:11 +0100

    Test: Minor clean up

--- norway-law-java/src/test/java/no/law/NorwegianLawTextName_to_LawIdTest.java
+++ norway-law-java/src/test/java/no/law/NorwegianLawTextName_to_LawIdTest.java
@@ -1,43 +1,32 @@
 package no.law;
 
-import no.law.lawreference.NorwegianLawTextName_to_LawId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static no.law.lawreference.NorwegianLawTextName_to_LawId.law;
 
 public class NorwegianLawTextName_to_LawIdTest {
 
     @Test
     public void lawName_negative_invalid() {
-        Assertions.assertNull(NorwegianLawTextName_to_LawId.law(""));
+        Assertions.assertNull(law(""));
     }
 
     @Test
     public void lawName_negative_nullInput() {
-        Assertions.assertThrows(NullPointerException.class, () -> {
-            NorwegianLawTextName_to_LawId.law(null);
-        });
+        Assertions.assertThrows(NullPointerException.class, () -> law(null));
     }
 
     @Test
     public void lawName_negative_invalidMonth() {
-        Assertions.assertThrows(NullPointerException.class, () -> {
-            Assertions.assertNull(NorwegianLawTextName_to_LawId.law("lov 20. june 2014 nr. 49"));
-        });
+        Assertions.assertThrows(NullPointerException.class, () ->
+                Assertions.assertNull(law("lov 20. june 2014 nr. 49")));
     }
 
     @Test
     public void lawName() {
-        Assertions.assertEquals(
-                "LOV-2014-06-20-49",
-                NorwegianLawTextName_to_LawId.law("lov 20. juni 2014 nr. 49")
-        );
-        Assertions.assertEquals(
-                "LOV-2006-05-19-16",
-                NorwegianLawTextName_to_LawId.law("offentleglova")
-        );
-        Assertions.assertEquals(
-                "LOV-2006-05-19-16",
-                NorwegianLawTextName_to_LawId.law("Offentleglova (2006)")
-        );
+        Assertions.assertEquals("LOV-2014-06-20-49", law("lov 20. juni 2014 nr. 49"));
+        Assertions.assertEquals("LOV-2006-05-19-16", law("offentleglova"));
+        Assertions.assertEquals("LOV-2006-05-19-16", law("Offentleglova (2006)"));
     }
 }
